b3f778d3214e7171f84b54b3ce3da51b9a86bb68
Spencer Blanden <89828067+spencerblanden@users.noreply.github.com>
2021-09-22 22:39:24 -0500

    most functionality is there

--- js/script.js
+++ js/script.js
@@ -1,34 +1,75 @@
 $(function(){
 
-    window.onload = function(event) {
-        newGame(event);
-      };
-
+//     window.onload = function(event) {
+//         render(event);
+//       };
+myStorage = window.localStorage;
 
 //Global Variables
     const BASE_URL = 'https://pokeapi.co/';
     let idNum = Math.floor(Math.random() * 100);
-    let pokeWeight1 = 0;
-    let pokeWeight2 = 0;
     let pokeData;
     let correct=0;
-    let incorrect=0;    
+    let incorrect=0;  
+    let generation = 1;  
 
+    
+    
+
+    //Poke Heavy Variables
+    let pokeWeight1 = 0;
+    let pokeWeight2 = 0;
+
+    //Poke Height Variables
+    let pokeHeight1 = 0;
+    let pokeHeight2 = 0;
+
+    let randomizedChoiceTypes = [];
+      let actualType;
 
 //HTML Element Variable assignment
-    let $name1 =$('#leftPoke');
-    let $name2 =$('#rightPoke');
-    let $outcome = $('#response');
-    let $inputNew = $('#midBtn');
-    let $inputLeft = $('#btnLeft')
-    let $inputRight = $('#btnRight')
-    let $correct = $('#correct')
-    let $inCorrect = $('#incorrect')
+let $outcome = $('#response');
+let $inputNew = $('.midBtn');
+let $inputNewMC = $('#btnMCStart')
+let $inputLeft = $('#btnLeft')
+let $inputRight = $('#btnRight')
+let $correct = $('#correct')
+let $inCorrect = $('#incorrect')
+let $inputHL =$('#btnLeft_height')
+let $inputHR =$('#btnRight_height')
+let $reset =$('#btnMCReset')
+
+let $gen1 = $('#btnGen1')
+let $gen2 = $('#btnGen2')
+let $gen3 = $('#btnGen3')
+let $gen4 = $('#btnGen4')
+let $genAll = $('#btnGen0')
+
+
+
+//Poke Heavy HTML Element Variable Assignment
+let $name1 =$('#leftPoke');
+let $name2 =$('#rightPoke');
+
+//Poke Height HTML ELement Variable Assignment
+let $name_choices =$('.pkmn_choices');
+
+
 
 // buttons
 $inputNew.on('click', newGame)
-$inputLeft.on('click', result1)
-$inputRight.on('click', result2)
+$inputLeft.on('click', function(){result1(pokeWeight1, pokeWeight2)})
+$inputRight.on('click', function(){result2(pokeWeight1, pokeWeight2)})
+$inputHL.on('click', function(){result1(pokeHeight1, pokeHeight2)})
+$inputHR.on('click', function(){result2(pokeHeight1, pokeHeight2)})
+$inputNewMC.on('click', handleGetDataType)
+$reset.on('click', clearScore)
+
+$gen1.on('click', function(){setGen(1)})
+$gen2.on('click', function(){setGen(2)})
+$gen3.on('click', function(){setGen(3)})
+$gen4.on('click', function(){setGen(4)})
+$genAll.on('click', function(){setGen(0)})
 
 
 
@@ -38,17 +79,74 @@ $inputRight.on('click', result2)
         handleGetData1(event);
         handleGetData2(event);
     }
+
+    function newMP(event) {
+       createMP(event);
+       $reset.empty();
+       $outcome.empty();
+       $reset.append(`<button id='btnMCReset' type="submit" >Reset?</button>`)
+       $inputNewMC.hide();
+    }
+
+    // get data functions
+        function handleGetData1(event) {
+            event.preventDefault();
+            const gennn = localStorage.getItem('generation');
+            getIdNum(gennn);
+        
+            $.ajax(`${BASE_URL}api/v2/pokemon/${idNum}/`).then(function(data) {
+                pokeData = data;
+                console.log(data)
+               render1();
+               pokeWeight1 = pokeData.weight;
+               pokeHeight1 = pokeData.height;
+            }, function (error) {
+                console.log(error);
+            } );
+        }
+        function handleGetData2(event) {
+            event.preventDefault();
+            const gennn = localStorage.getItem('generation');
+            getIdNum(gennn);
     
-    function result1() {
+        
+            $.ajax(`${BASE_URL}api/v2/pokemon/${idNum}/`).then(function(data) {
+                pokeData = data;
+                console.log(data)
+               render2();
+               pokeWeight2 = pokeData.weight;
+               pokeHeight2 = pokeData.height;
+               console.log(pokeHeight2)
+            }, function (error) {
+                console.log(error);
+            } );
+        }
+        function handleGetDataType(event) {
+            event.preventDefault();
+            const gennn = localStorage.getItem('generation');
+            getIdNum(gennn);
+        
+            $.ajax(`${BASE_URL}api/v2/pokemon/${idNum}/`).then(function(data) {
+                pokeData = data;
+                console.log(data)
+               render1();
+               newMP(event);
+            }, function (error) {
+                console.log(error);
+            } );}
+
+
+    
+    function result1(pokePoint1, pokePoint2) {
         console.log('1push')
-        if (pokeWeight1 > pokeWeight2){
+        if (pokePoint1 > pokePoint2){
             outcome = "You're a Pokemon Expert";
             $outcome.append( `
             <p id='response'>${outcome}</p>
             <button type='button' class='gameOn'>Keep going?</button>`)
             $outcome.show();
             addCorrect(1);
-        } else if (pokeWeight1 < pokeWeight2) {
+        } else if (pokePoint1 < pokePoint2) {
             outcome = "Oh dang! You're wrong.";
             $outcome.append( `
            <p id='response'>${outcome}</p>
@@ -65,17 +163,17 @@ $inputRight.on('click', result2)
     $gameAgain.on('click', cleanUp)
 }
     
-    function result2() {
+    function result2(pokePoint1, pokePoint2) {
         console.log('2push')
         let outcome = ('');
-        if (pokeWeight2 > pokeWeight1){
+        if (pokePoint2 > pokePoint1){
             outcome = "You're a Pokemon Expert";
             $outcome.append( `
             <p id='response'>${outcome}</p>
             <button type='button' class='gameOn'>Keep going?</button>`)
             $outcome.show();
             addCorrect(1);
-        } else if (pokeWeight2 < pokeWeight1) {
+        } else if (pokePoint2 < pokePoint1) {
         outcome = "Oh dang! You're wrong.";
         $outcome.append( `
         <p id='response'>${outcome}</p>
@@ -97,40 +195,51 @@ $inputRight.on('click', result2)
         newGame(event);
     }
 
-// get data functions
-    function handleGetData1(event) {
-        event.preventDefault();
-        getIdNum();
-    
-        $.ajax(`${BASE_URL}api/v2/pokemon/${idNum}/`).then(function(data) {
-            pokeData = data;
-            console.log(data)
-           render1();
-           pokeWeight1 = pokeData.weight;
-           console.log(pokeWeight1)
-        }, function (error) {
-            console.log(error);
-        } );
+    function setGen(num) {
+        console.log(num)
+        if (num === 0){
+            localStorage.setItem('generation', 0)
+    }else if(num === 1) {
+        localStorage.setItem('generation', 1)
+    }else if(num === 2) {
+        localStorage.setItem('generation', 2)
+    }else if(num === 3) {
+        localStorage.setItem('generation', 3)
+    }else if(num === 4) {
+        localStorage.setItem('generation', 4)
     }
-    function handleGetData2(event) {
-        event.preventDefault();
-        getIdNum();
-
-    
-        $.ajax(`${BASE_URL}api/v2/pokemon/${idNum}/`).then(function(data) {
-            pokeData = data;
-            console.log(data)
-           render2();
-           pokeWeight2 = pokeData.weight;
-           console.log(pokeWeight2)
-        }, function (error) {
-            console.log(error);
-        } );
+    console.log(localStorage.getItem('generation'))
+    let genNow;
+    if (num === 0) {
+        genNow = 'all';
+    } else if(num === 1){
+        genNow = 'one';
+    } else if(num === 2){
+        genNow = 'two';
+    } else if(num === 3){
+        genNow = 'three';
+    } else if(num === 4){
+        genNow = 'four';
     }
-    
+    else {console.log('bigerror')}
+    $correct.empty();
+        $correct.text(`Current Gen: ${genNow}`)
+    }
 
-    function getIdNum() {
-        idNum = Math.floor(Math.random() * 100);
+    function getIdNum(num) {
+        console.log(num)
+        if (num == 0){
+            idNum = Math.floor(Math.random() * 493);
+        }else if(num == 1) {
+            idNum = Math.floor(Math.random() * 151);
+            console.log('what?')
+        }else if(num == 2) {
+            idNum = Math.floor(Math.random()*(251-152+1)+152);
+        }else if(num == 3) {
+            idNum = Math.floor(Math.random()*(386-252+1)+252);
+        }else if(num == 4) {
+            idNum = Math.floor(Math.random()*(492-387+1)+387);
+        }
     }
 
 // render functions
@@ -161,4 +270,165 @@ $inputRight.on('click', result2)
         $inCorrect.empty();
         $inCorrect.text(`Incorrect: ${incorrect}`)
     }
+   
+    // Category code
+    function createMP(event) {
+        let pokeTypes= ['normal','fire','water','grass',
+                        'electric','ice','fighting','poison',
+                        'ground','flying','psychic','bug',
+                        'rock','ghost','dark','dragon',
+                        'steel','fairy' ];
+
+      
+        let typeGroup = getType(pokeData);
+        console.log(typeGroup)
+        let num= typeGroup.length;
+        
+        if (num === 2){
+            let type1 = typeGroup[0]
+            let type2 = typeGroup[1]
+            typeIndex= pokeTypes.indexOf(type1)
+            actualType=pokeTypes[typeIndex]
+            pokeTypes.splice(typeIndex, 1)
+            typeIndex2= pokeTypes.indexOf(type2)
+            let actualType2=pokeTypes[typeIndex2]
+            pokeTypes.splice(typeIndex2, 1)
+            console.log(actualType, actualType2)
+            console.log(pokeTypes)
+            } else {
+                let type1 = typeGroup[0]
+                typeIndex= pokeTypes.indexOf(type1)
+            actualType=pokeTypes[typeIndex]
+            pokeTypes.splice(typeIndex, 1)
+            console.log(actualType)
+            console.log(pokeTypes)
+            }
+            let randNum =Math.floor(Math.random() * 16)
+            let choiceTypes = []
+            choiceTypes.push(pokeTypes.splice(randNum, 1))
+            randNum =Math.floor(Math.random() * 15)
+          
+            choiceTypes.push(pokeTypes.splice(randNum, 1))
+            randNum =Math.floor(Math.random() * 14)
+            
+            choiceTypes.push(pokeTypes.splice(randNum, 1))
+            choiceTypes.push(actualType)
+
+            console.log(choiceTypes)
+
+            
+            randomizedChoiceTypes = shuffle(choiceTypes)
+
+            $name_choices.empty();
+            $name_choices.append(`<button type="button" class='pkmn_c' id='btnA'>A</button>
+            <p class='choice'>${randomizedChoiceTypes[0]}</p>
+            <button type="button" class='pkmn_c' id='btnB'>B</button>
+            <p class='choice'>${randomizedChoiceTypes[1]}</p>
+            <button type="button" class='pkmn_c' id='btnC'>C</button>
+            <p class='choice'>${randomizedChoiceTypes[2]}</p>
+            <button type="button" class='pkmn_c' id='btnD'>D</button>
+            <p class='choice'>${randomizedChoiceTypes[3]}</p>
+            `)
+            let $choiceA = $('#btnA');
+            let $choiceB = $('#btnB');
+            let $choiceC = $('#btnC');
+            let $choiceD = $('#btnD');
+            $choiceA.on('click', function(){mpCheck(randomizedChoiceTypes[0], actualType)})
+            $choiceB.on('click', function(){mpCheck(randomizedChoiceTypes[1], actualType)})
+            $choiceC.on('click', function(){mpCheck(randomizedChoiceTypes[2], actualType)})
+            $choiceD.on('click', function(){mpCheck(randomizedChoiceTypes[3], actualType)})
+
+
+        }
+
+
+        function mpCheck(choice, proof) {
+            console.log('2push')
+            let outcome = ('');
+            if (choice === proof){
+                console.log('yes')
+                outcome = "You're a Pokemon Expert";
+                $outcome.append( `
+                <p id='response'>${outcome}</p>
+                <button type='button' class='gameOn'>Keep going?</button>`)
+                $outcome.show();
+                addCorrect(1);
+            } else if (choice !== proof) {
+            outcome = "Oh dang! You're wrong.";
+            $outcome.append( `
+            <p id='response'>${outcome}</p>
+            <button type='button' class='gameOn'>Try again?</button>`)
+            $outcome.show();
+            addIncorrect(1);
+        } else {
+            outcome = 'error'
+            $outcome.append( `
+                <p id='response'>${outcome}</p>`)
+                $outcome.show();
+        } 
+        let $gameAgain = $('.gameOn');
+        $gameAgain.on('click', cleanUpType)
+    }
+
+    function cleanUpType(event) {
+        $outcome.empty()
+        handleGetDataType(event);
+        newMP();
+    }
+
+        function shuffle(array) {
+            let currentIndex = array.length,  randomIndex;
+          
+            while (currentIndex != 0) {
+          
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex--;
+          
+              [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+            }
+          
+            return array;
+          }
+    
+    function getType(data) {
+        let typeGroup = [];
+    
+    let currentType = data.types;
+    let currType1 = currentType.slice(0,1);
+  
+    let pokeType1 = currType1[0];
+    let type1 = pokeType1.type['name']
+   typeGroup.push(type1)
+
+
+   if (currentType.length === 2) {
+
+    let currType2= currentType.slice(1,2);
+
+    let pokeType2 = currType2[0];
+    
+
+        let type2 = pokeType2.type['name']
+        typeGroup.push(type2)
+        console.log(typeGroup)
+
+    } 
+    return typeGroup;
+    }
+
+    function clearScore(){
+        correct = 0;
+        $correct.empty();
+        $correct.text(`Correct: 0`)
+        incorrect = 0;
+        $inCorrect.empty();
+        $inCorrect.text(`Incorrect: 0`)
+
+        handleGetDataType(event)
+    }
     });
+
+
+
+    
